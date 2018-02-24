471b93001ca664981f52c00fc38bfdf4d968f5ae
Krzysiek Lipka <christophorus.lipca@gmail.com>
2018-02-24 12:30:59 +0100

    dat types prymitive

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 console.log('Hello Krzysiek!')
 
-var textVariable = "Krzysiek"
+//data types
 
+//prymitives
+
+
+//string
+var textVariable = "Krzysiek"
 console.log(textVariable)
+
+//number
+var numberVariable = 43
+console.log(numberVariable)
+
+//boolean
+var boolVariable = true
+console.log(boolVariable)
+
+//null
+var nullVariable = null
+console.log(nullVariable)
+
+//undefined
+var undefinedVariable = undefined
+console.log(undefinedVariable)
+
+
+
+
+
