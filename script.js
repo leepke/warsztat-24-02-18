d19d57706291371bfe9aac3749b44f9651ada9ff
Krzysiek Lipka <christophorus.lipca@gmail.com>
2018-02-24 16:07:10 +0100

    killing mole

--- script.js
+++ script.js
@@ -1,5 +1,58 @@
 (function(){
 
+    var points
+    var time
+    var mole
+
+    function addPoint(){
+        points++
+        console.log('Dodano punkt!', points)
+        displayPoints(points)
+    }
+
+    function displayPoints(pointsParam){
+        var pointsContainer = document.querySelector('.points')
+        pointsContainer.innerText = pointsParam
 }
 
-)()
+
+    function makeMole(){
+
+            var molePosX = Math.round(
+                Math.random() * (window.innerWidth - window.innerHeight / 10)
+            )
+            var molePosY = Math.round(
+                Math.random() * (window.innerHeight - window.innerWidth / 10)
+            )
+
+            var mole = document.createElement('div')
+
+            mole.classList.add('mole')
+
+            mole.style.left = molePosX + 'px'
+            mole.style.top = molePosY + 'px'
+
+            mole.addEventListener(
+                'click',
+                function(){
+                    mole.remove()
+                    addPoint()
+                }
+            )
+
+            document.querySelector('body').appendChild(mole)
+
+            return mole
+    }
+
+    function init(){
+        points = 0
+        time = 10
+        mole = makeMole()
+
+        displayPoints(points)
+    }
+    
+    init()
+
+})()
